297dcdb6e460c643b4d9b5ad8a9005fca29b62e2
Safdar077 <136257273+Safdar077@users.noreply.github.com>
2025-11-29 17:29:28 +0530

    Add structured data for SEO enhancement
    Added structured data scripts for BreadcrumbList and LocalBusiness.

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,6 +18,41 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="google-site-verification"
         content="566672fc59c1767e" />
+        <script type="application/ld+json">
+    {`
+      {
+        "@context": "https://schema.org",
+        "@type": "BreadcrumbList",
+        "itemListElement": [
+          {
+            "@type": "ListItem",
+            "position": 1,
+            "name": "Home",
+            "item": "https://technova-digital.vercel.app/"
+          }
+        ]
+      }
+    `}
+  </script>
+
+  <script type="application/ld+json">
+    {`
+      {
+        "@context": "https://schema.org",
+        "@type": "LocalBusiness",
+        "name": "Tech Nova Digital",
+        "url": "https://technova-digital.vercel.app",
+        "image": "https://technova-digital.vercel.app/logo.png",
+        "telephone": "+918983939755",
+        "address": {
+          "@type": "PostalAddress",
+          "addressLocality": "Pune",
+          "addressRegion": "Maharashtra",
+          "addressCountry": "IN"
+        }
+      }
+    `}
+  </script>
       </head>
       <body className={`font-sans antialiased`}>
         {children}
